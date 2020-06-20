6bad26c459b9a6c2d284be781d7ff1f7c51e14b8
Taehwa Lee <xoghk0321@gmail.com>
2020-06-20 21:36:01 +0900

    feat 구매할 로또 갯수, 수동 갯수를 입력 받는다

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,40 @@ import * as React from 'preact';
 import { useState } from 'preact/hooks';
 
 const App = () => {
+  const [lottoAmount, setlottoAmount] = useState(0);
+  const [doneSettingLottoAmount, setDoneSettingLottoAmount] = useState(false);
+  const [manualLottoAmount, setManualLottoAmount] = useState(0);
+  const [doneSettingManualLottoAmount, setDoneSettingManualLottoAmount] = useState(false);
+
+  const handleChangeLottoAmount = (e: any): void => setlottoAmount(e.target.value);
+  const handleChangeManualLottoAmount = (e: any): void => setManualLottoAmount(e.target.value);
+
+  const handleClickLottoAmountBtn = (e: any): void => {
+    e.preventDefault();
+    setDoneSettingLottoAmount(true)
+  }
+
+  const handleClickManualLottoAmountBtn = (e: any): void => {
+    e.preventDefault();
+    setDoneSettingManualLottoAmount(true)
+  }
+
   return (
     <div>
-      
+      <div>
+        <label for="lottoAmount">로또 몇 장을 구매하시겠어요?</label>
+        <input id="lottoAmount" type="text" value={ lottoAmount } 
+          onChange={ handleChangeLottoAmount }></input>
+        <button type="submit" onClick={ handleClickLottoAmountBtn }>확인</button>
+      </div>
+      { doneSettingLottoAmount && (
+      <div>
+        <label for="manualLottoAmount">몇 장을 수동으로 할까요?</label>
+        <input id="manualLottoAmount" type="text" value={ manualLottoAmount } 
+          onChange={ handleChangeManualLottoAmount }></input>
+        <button type="submit" onClick={ handleClickManualLottoAmountBtn }>확인</button>
+      </div>
+      )}
     </div>
   );
 }
